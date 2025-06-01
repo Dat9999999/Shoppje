ddbf5098d3f2ccb3aa3a19a5e63d6f2566e7a2e5
Dat9999999 <huynhtandat184@gmail.com>
2025-06-01 14:52:47 +0700

    admin view list category

---
+++ Shoppje/Areas/admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoppje.Services.interfaces;
+
+namespace Shoppje.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+        public IActionResult Index()
+        {
+            var categories = _categoryService.GetAll().Result;
+            _logger.LogInformation("CategoryController: Index - Categories Count: {Count}", categories.Count());
+            return View(categories);
+        }
+    }
+}
